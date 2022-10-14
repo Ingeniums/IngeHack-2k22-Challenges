2bec2b5845dcea8da93956c6ce01904f70257227
vvxhid <wahidmadano@gmail.com>
2022-10-14 21:02:10 +0000

    update werev

--- web/werev/app/server/index.php
+++ web/werev/app/server/index.php
@@ -1,3 +1,8 @@
 <?php
-
-var_dump(inge_gateway());
+if(isset($_GET['file']) && $_GET['file'] != '' && is_string($_GET['file'])) {
+    $x = file_get_contents($_GET['file']);
+    echo $x;
+    die();
+}
+phpinfo();
+;
